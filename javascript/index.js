2d893f46b862cc53c093a17bdbd9b0d4e8ed4468
Zeynep Deniz <zeynep351961@gmail.com>
2023-01-28 17:36:51 +0300

    course

---
+++ javascript/index.js
@@ -0,0 +1,19 @@
+const myName = "Brian Holt";
+console.log(myName);
+
+const firstName = "Brian";
+const lastName = "Holt";
+
+const sentence = "Hello " + firstName + " " + lastName + "! How are you!?";
+const sentenceWithTemplate = `Hello ${firstName} ${lastName}! How are you!?`;
+
+console.log(sentence);
+console.log(sentenceWithTemplate);
+
+const skyIsBlue = true;
+
+if (skyIsBLue) {
+  console.log("The sky is blue?");
+} else {
+  console.log("The sky is ... not blue?");
+}
---
+++ javascript/main.js
@@ -0,0 +1,19 @@
+let monthlyRent = 500;
+
+monthlyRent = 1;
+
+const yearlyRent = monthlyRent * 12;
+console.log(yearlyRent);
+
+// let'de değişken sabit değildir. kendi scope'u dışında işlev görmez.
+// var ile aralarındaki var ise var'ın bütün her yerde geçerli olması
+// let'in ise kendi scope'u içerisinde geçerli olmasıdır.
+
+//const monthlyRent = 500;
+
+//monthlyRent = 1;
+
+//const yearlyRent = monthlyRent * 12;
+//console.log(yearlyRent);
+
+//hata verir. const'da değişken sabittir.
