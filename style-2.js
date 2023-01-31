43abca71a5e3092f1e3ffc539952392367d7a2da
Zeynep Deniz <zeynep351961@gmail.com>
2023-01-31 19:38:17 +0300

    course

--- style-2.js
+++ style-2.js
@@ -1,6 +1,8 @@
 const input = document.querySelector(".color-input");
 const paragraph = document.querySelector(".color-box");
 
+//////////////////
+
 input.addEventListener("change", function () {
   paragraph.style.backgroundColor = input.value;
 });
--- style.js
+++ style.js
@@ -9,3 +9,11 @@ const paragraph = document.querySelector(".p-to-copt-to");
 input.addEventListener("keyup", function () {
   paragraph.innerText = input.value;
 });
+
+/////////////////////
+
+document
+  .querySelector(".button-container")
+  .addEventListener("click", function (event) {
+    alert(`You clicked on button ${event.target.innerText}`);
+  });
